29b1820f81b2001e9ba5844dc8d71bd5c8b30ce4
ebana <ebaerdenebat39@gmail.com>
2025-03-31 09:59:28 +0800

    navbar css uurcluw

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,21 +23,21 @@ function App() {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           placeholder="Ном хайх..."
-          className="p-2 rounded-md text-black bg-gray-300 w-fit xs:w-20 focus:w-30"
+          className="p-2 rounded-md text-black bg-gray-300 transition-all duration-0.5 w-fit xs:w-20 focus:w-30 "
         />
       </motion.header>
       {/* Main Content */}
       <motion.main
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5 }}
+        initial={{ opacity: 0, x: -60 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6 }}
         className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
       >
         {['Сургалтын материал', 'Эрдэм шинжилгээ', 'Цахим ном'].map((category, index) => (
           <motion.div
             key={index}
-            whileHover={{ scale: 1.05 }}
-            className="bg-white p-4 rounded-xl shadow-md hover:shadow-lg transition-all"
+            whileHover={{ scale: 1.02 }}
+            className="bg-white p-4 mr-2 rounded-xl shadow-md hover:shadow-lg transition-all duration-300"
           >
             <h2 className="text-xl font-semibold">{category}</h2>
             <p className="text-gray-700 mt-2">Энд номын тайлбар орно.</p>
